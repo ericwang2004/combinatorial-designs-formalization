fc0443414aa786fd29b31291776951dc56448767
Eric Wang <ericwang@gmail.com>
2025-06-02 00:48:01 -0400

    def of r

--- BIBD/Basic.lean
+++ BIBD/Basic.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Set.Basic
 import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Finset.Card
 import Mathlib.Data.Fintype.Basic
 import Mathlib.Data.Fintype.Card
 import Mathlib.Data.Multiset.Basic
@@ -17,7 +18,9 @@ structure BIBD (X : Type u) [Fintype X] [DecidableEq X] extends Design X where
   v : ℕ
   k : ℕ
   l : ℕ
+  r : ℕ
   hvk : v > k ∧ k ≥ 2
+  hr : r * (k - 1) = l * (v - 1)
   hX : card X = v
   hA : ∀ B ∈ blocks, Finset.card B = k
   balance : ∀ x y : X, x ≠ y →
@@ -25,9 +28,11 @@ structure BIBD (X : Type u) [Fintype X] [DecidableEq X] extends Design X where
 
 variable {X : Type u} [Fintype X] [DecidableEq X]
 
-def pointOccurrences (Φ : BIBD X) (x : X) :=
+def rep (Φ : BIBD X) (x : X) :=
   countP (fun B ↦ x ∈ B) Φ.blocks
 
-theorem pointOccurrences_constant (Φ : BIBD X) :
-    ∃ r, ∀ x : X, pointOccurrences Φ x = r := by
+theorem rep_constant (Φ : BIBD X) :
+    ∀ x : X, rep Φ x = Φ.r := by
+  intro x
+  let I := {(y, A) | (y ≠ x) ∧ A ∈ Φ.blocks ∧ x ∈ A ∧ y ∈ A}
   sorry
