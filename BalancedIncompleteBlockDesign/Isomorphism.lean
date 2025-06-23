e1ed17b20b66e2e4a1020cb284ef4cefbfc88cf0
Eric Wang <ericwang@gmail.com>
2025-06-23 16:20:42 -0400

    action on j-subsets

--- BalancedIncompleteBlockDesign/Isomorphism.lean
+++ BalancedIncompleteBlockDesign/Isomorphism.lean
@@ -1,6 +1,7 @@
 import BalancedIncompleteBlockDesign.IncidenceMatrix
+import Mathlib.GroupTheory.GroupAction.Quotient
 
-open BalancedIncompleteBlockDesign Finset
+open BalancedIncompleteBlockDesign Finset MulAction
 namespace BalancedIncompleteBlockDesign
 
 variable {X : Type*} [Fintype X] [DecidableEq X]
@@ -61,3 +62,58 @@ theorem perm_of_iso (Φ₁ : Design X b) (Φ₂ : Design Y b)
         (Finset.ext_iff.mp h) _
       have := funext_iff.mp iso.map_blocks j
       rwa [Function.comp] at this
+
+variable {G : Type*} [Fintype G] [Group G] [MulAction G X]
+
+abbrev jSubsets (j : ℕ) (X : Type*) := { s : Finset X // s.card = j }
+
+instance : SMul G (Finset X) where
+  smul g S := image (g • ·) S
+
+instance (j : ℕ) : SMul G (jSubsets j X) where
+  smul g S := ⟨g • S.val, by
+    calc
+      #(g • S.val) = #(image (fun x ↦ g • x) S.val) := rfl
+      _ = #S.val := by
+        rw [card_image_of_injective]
+        exact MulAction.injective _
+      _ = j := S.property
+    ⟩
+
+omit [Fintype X] [Fintype G] in
+theorem mem_smul_jset {j : ℕ} {S : jSubsets j X} {g : G} {x : X} :
+    x ∈ (g • S).val ↔ ∃ w ∈ S.val, g • w = x := by
+  constructor <;> intro hx
+  · have : x ∈ image (fun x ↦ g • x) S.val := hx
+    rwa [Finset.mem_image] at this
+  · have : (g • S).val = image (fun x ↦ g • x) S.val := rfl
+    rwa [this, Finset.mem_image]
+
+instance (j : ℕ) : MulAction G (jSubsets j X) where
+  one_smul S := by
+    apply Subtype.ext
+    ext x
+    constructor <;> intro hx
+    · rw [mem_smul_jset] at hx
+      obtain ⟨w, hw₁, hw₂⟩ := hx
+      rw [one_smul] at hw₂
+      rwa [←hw₂]
+    · rw [mem_smul_jset]
+      exact ⟨x, hx, by rw [one_smul]⟩
+  mul_smul x y S := by
+    apply Subtype.ext
+    ext a
+    constructor <;> intro ha <;> rw [mem_smul_jset] at ha ⊢ <;>
+      obtain ⟨w, hw₁, hw₂⟩ := ha
+    · use y • w
+      constructor
+      · rw [mem_smul_jset]
+        use w
+      · rwa [smul_smul]
+    · rw [mem_smul_jset] at hw₁
+      obtain ⟨z, hz₁, hz₂⟩ := hw₁
+      exact ⟨z, hz₁, by rw [←hw₂, ←hz₂, smul_smul]⟩
+
+def jOrbits (G : Type*) [Fintype G] [Group G] [∀ j, MulAction G (jSubsets j X)]
+    (X : Type*) [Fintype X] [MulAction G X] (j : ℕ) :=
+  Quotient |> orbitRel G (jSubsets j X) -- doesn't work
