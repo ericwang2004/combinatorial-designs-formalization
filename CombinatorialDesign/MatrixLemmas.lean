d740fb3ae3158bf3c73639f856957fdda9b3e94f
ElifUskuplu <uskupluelif@gmail.com>
2026-01-25 22:19:14 -0500

    very small proof of `det_one_add_column_mul_row`

--- CombinatorialDesign/MatrixLemmas.lean
+++ CombinatorialDesign/MatrixLemmas.lean
@@ -1,7 +1,9 @@
 import Mathlib.Data.Matrix.Rank
 import Mathlib.Data.Finset.Basic
+
 import Mathlib.NumberTheory.SumFourSquares
 import Mathlib.LinearAlgebra.Matrix.Block
+import Mathlib.LinearAlgebra.Matrix.SchurComplement
 
 open Matrix Finset
 
@@ -14,33 +16,17 @@ def isZeroOne {m n α : Type*} [One α] [Zero α] (M : Matrix m n α) : Prop :=
 /-- ### Matrix Determinant Lemma -/
 theorem det_one_add_column_mul_row {α n : Type*} [CommRing α] [Fintype n] [DecidableEq n]
     (u : Matrix n (Fin 1) α) (v : Matrix (Fin 1) n α) : det (1 + u * v) = 1 + (v * u) 0 0 := by
-  let A := fromBlocks (1 : Matrix n n α) 0 v (1 : Matrix (Fin 1) (Fin 1) α)
-  let B := fromBlocks (1 + u * v) u 0 (1 : Matrix (Fin 1) (Fin 1) α)
-  let C := fromBlocks (1 : Matrix n n α) 0 (-v) (1 : Matrix (Fin 1) (Fin 1) α)
-  let D := fromBlocks (1 : Matrix n n α) u 0 (1 + v * u)
-  have detA : det A = 1 := by
-    simp only [A, det_fromBlocks_zero₁₂, det_one, mul_one]
-  have detB : det B = det (1 + u * v) := by
-    simp only [B, det_fromBlocks_zero₂₁, det_unique, one_apply_eq, mul_one]
-  have detC : det C = 1 := by
-    simp only [C, det_fromBlocks_zero₁₂, det_one, det_unique, one_apply_eq, mul_one]
-  have detD : det D = 1 + (v * u) 0 0 := by
-    simp only [det_fromBlocks_zero₂₁, det_one, det_unique, Fin.default_eq_zero, Fin.isValue,
-      add_apply, one_apply_eq, one_mul, D]
-  have hABCD : A * B * C = D := by
-    simp only [A, B, C, D, fromBlocks_multiply, one_mul, Matrix.mul_zero, add_zero,
-      mul_one, Matrix.one_mul, Matrix.mul_one, Matrix.mul_neg, add_neg_cancel_right,
-      zero_add, fromBlocks_inj, true_and]
-    constructor
-    · rw [Matrix.mul_add, Matrix.mul_one, Matrix.add_mul, neg_add, Matrix.one_mul, add_comm v,
-        add_assoc, add_comm _ (-v), ←add_assoc v, add_neg_cancel, zero_add, ←Matrix.mul_assoc,
-        add_neg_cancel]
-    · rw [add_comm]
-  calc
-    det (1 + u * v) = det A * det B * det C := by rw [detA, detB, detC, one_mul, mul_one]
-    _ = det (A * B * C) := by simp only [det_mul]
-    _ = det D := by rw [hABCD]
-    _ = 1 + (v * u) 0 0 := by rw [detD]
+  let u' : n → α := fun i => u i 0
+  let v' : n → α := fun j => v 0 j
+  have hu : u = replicateCol (Fin 1) u' := by
+    ext i j
+    fin_cases j
+    simp [u', replicateCol]
+  have hv : v = replicateRow (Fin 1) v' := by
+    ext i j
+    fin_cases i
+    simp [v', replicateRow]
+  simp only [hv, hu, det_one_add_replicateCol_mul_replicateRow, replicateRow_mul_replicateCol_apply]
 
 theorem det_ones_add_diagonal {α : Type*} (n : Type*) [Field α] [Fintype n] [DecidableEq n]
     (a b : α) (hb : b ≠ 0) :
@@ -50,15 +36,14 @@ theorem det_ones_add_diagonal {α : Type*} (n : Type*) [Field α] [Fintype n] [D
   have expr : a • allOnes n n α + b • 1 = b • (1 + ((a / b) • u) * v) := by
     ext
     simp only [add_apply, smul_apply, smul_eq_mul, smul_mul, one_apply, allOnes, of_apply,
-      mul_apply, mul_one, mul_ite, mul_zero, univ_unique, Fin.default_eq_zero, Fin.isValue,
-      sum_singleton, u, v]
+      mul_apply, mul_one, mul_zero, univ_unique, sum_singleton, u, v]
     rw [mul_add, mul_ite, mul_one, mul_zero, add_comm, mul_div_cancel₀ _ hb]
   have v_mul_u : v * u = Fintype.card n • allOnes (Fin 1) (Fin 1) α := by
     ext
     simp only [mul_apply, allOnes, v, u, mul_one, sum_const, card_univ,
       nsmul_eq_mul, smul_apply, of_apply]
   have det_expr := (det_one_add_column_mul_row ((a / b) • u) v)
-  simp only [smul_mul, Fin.isValue, Matrix.mul_smul, smul_apply, smul_eq_mul,
+  simp only [smul_mul, Matrix.mul_smul, smul_apply, smul_eq_mul,
     v_mul_u, allOnes, smul_eq_mul, mul_one, of_apply, ] at det_expr
   simp only [nsmul_eq_mul, mul_one] at det_expr
   calc
